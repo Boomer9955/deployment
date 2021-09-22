8510fafc97790a1f8999089e4eb0957aeb9a7abc
Boomer9955 <rombich@bk.ru>
2021-09-22 11:49:46 +0300

    new gr

---
+++ groovy/Start.groovy
@@ -0,0 +1,39 @@
+#!/usr/bin/env groovy
+
+registry = "boomer9955/mydjango"
+env.registryCredential = 'dockerhub'
+dockerImage = ''
+env.credgitc='git'
+env.urlapp='https://github.com/Boomer9955/django-project.git'
+env.urlconf='https://github.com/Boomer9955/minikube.git'
+
+
+dir("${WORKSPACE}/config"){
+    git changelog: false, poll: false, credentialsId: "$env.credgitc", url: "$env.urlconf", branch: 'main'
+}
+
+stage('Building and push'){
+    if(newimages.toBoolean()){
+        dir("${WORKSPACE}/project"){
+            git changelog: false, poll: false, credentialsId: "$env.credgitc", url: "$env.urlapp", branch: 'main'
+            dir("${WORKSPACE}/project/django"){
+                sh "ls -al"
+                dockerImage = docker.build registry + ":${BUILD_NUMBER}"
+                docker.withRegistry( '', env.registryCredential){
+                    dockerImage.push()
+                }
+                sh """ docker rmi ${registry}:${BUILD_NUMBER} """
+            }
+        }
+    }
+}
+
+stage('server'){
+    dir("${WORKSPACE}/config"){
+        withCredentials([sshUserPrivateKey(credentialsId: 'myserverdjango', keyFileVariable: 'keyansible', passphraseVariable: '', usernameVariable: 'vagrant')]) {
+            withCredentials([usernamePassword(credentialsId: 'dockerhub', passwordVariable: 'dockerpassword', usernameVariable: 'dockeruser')]) {
+                sh """ansible-playbook --private-key ${keyansible} -u ${vagrant} -i yml/hosts.yml --extra-vars "ONEHOST=${hostserver} build_number=${BUILD_NUMBER} docker_login=${dockeruser} docker_pass=${dockerpassword}" yml/django.yml --tags ${tags}"""
+            }
+        }
+    }
+}
