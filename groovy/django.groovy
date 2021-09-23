aa564860e0c183d04c828914c7b3b482801d872a
Boomer9955 <rombich@bk.ru>
2021-09-23 11:51:56 +0300

    new put

--- groovy/django.groovy
+++ groovy/django.groovy
@@ -3,19 +3,19 @@
 registry = "boomer9955/mydjango"
 env.registryCredential = 'dockerhub'
 dockerImage = ''
-env.credgitc='git'
-env.urlapp='https://github.com/Boomer9955/django-project.git'
-env.urlconf='https://github.com/Boomer9955/minikube.git'
+env.credgitc='mygit'
+env.urlapp='https://github.com/Boomer9955/myprojects.git'
+env.urlconf='https://github.com/Boomer9955/deployment.git'
 
 
 dir("${WORKSPACE}/config"){
-    git changelog: false, poll: false, credentialsId: "$env.credgitc", url: "$env.urlconf", branch: 'main'
+    git changelog: false, poll: false, credentialsId: "$env.credgitc", url: "$env.urlconf", branch: 'application'
 }
 
 stage('Building and push'){
     if(newimages.toBoolean()){
         dir("${WORKSPACE}/project"){
-            git changelog: false, poll: false, credentialsId: "$env.credgitc", url: "$env.urlapp", branch: 'main'
+            git changelog: false, poll: false, credentialsId: "$env.credgitc", url: "$env.urlapp", branch: 'djangopostgres'
             dir("${WORKSPACE}/project/django"){
                 sh "ls -al"
                 dockerImage = docker.build registry + ":${BUILD_NUMBER}"
