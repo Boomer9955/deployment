b84afc20f681fb8255c0d2e0e601aebc7887df2d
Boomer9955 <73993803+Boomer9955@users.noreply.github.com>
2021-12-23 11:45:21 +0300

    Update helm.groovy

--- groovy/helm.groovy
+++ groovy/helm.groovy
@@ -30,9 +30,9 @@ stage('Building and push'){
 
 stage('server'){
     dir("${WORKSPACE}/config"){
-        withCredentials([sshUserPrivateKey(credentialsId: 'localhost', keyFileVariable: 'keyansible', passphraseVariable: '', usernameVariable: 'boomer')]) {
+        withCredentials([sshUserPrivateKey(credentialsId: 'myserverdjango', keyFileVariable: 'keyansible', passphraseVariable: '', usernameVariable: 'vagrant')]) {
             withCredentials([usernamePassword(credentialsId: 'dockerhub', passwordVariable: 'dockerpassword', usernameVariable: 'dockeruser')]) {
-                sh """ansible-playbook --private-key ${keyansible} -u ${boomer} -i yml/hosts.yml --extra-vars "ONEHOST=${hostserver} build_number=${BUILD_NUMBER} docker_login=${dockeruser} docker_pass=${dockerpassword} helm_command=${helm_command} name_space=${NameSpace}" yml/django.yml --tags ${tags}"""
+                sh """ansible-playbook --private-key ${keyansible} -u ${vagrant} -i yml/hosts.yml --extra-vars "ONEHOST=${hostserver} build_number=${BUILD_NUMBER} docker_login=${dockeruser} docker_pass=${dockerpassword} helm_command=${helm_command} name_space=${NameSpace}" yml/django.yml --tags ${tags}"""
             }
         }
     }
