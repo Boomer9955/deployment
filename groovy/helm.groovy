d4389b59530b46254367fed6a12384eb7cbb5721
Boomer9955 <rombich@bk.ru>
2021-12-27 13:11:51 +0300

    _

--- groovy/helm.groovy
+++ groovy/helm.groovy
@@ -50,9 +50,11 @@ stage('New version Helm chart'){
             }
             sh "helm package prdjango/ --destination .deploy"
             git changelog: false, poll: false, credentialsId: "$env.credgitc", url: "$env.urlchart", branch: 'gh-pages'
-            withCredentials([usernamePassword(credentialsId: 'mygit', passwordVariable: 'gitpass', usernameVariable: 'gitlogin')]) {
+            withCredentials([usernamePassword(credentialsId: 'helmchart', passwordVariable: 'gitpass', usernameVariable: 'gitlogin')]) {
                 sh "cr upload -o ${gitlogin} -r helmcharts -p .deploy -t ${gitpass}"
                 sh "cr index -o ${gitlogin} --charts-repo https://boomer9955.github.io/helmcharts/ --git-repo helmcharts --package-path .deploy --token ${gitpass} -i index.yaml"
+                sh """git add * && git commit -m '${BUILD_NUMBER}'"""
+                sh """ git push $env.urlchart"""
             }
         }
     }
